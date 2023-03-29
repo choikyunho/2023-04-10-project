80c7c2cc44a0eeaff5c937d7e4a7feccf1925a53
h662Olbm <h662@olbm.app>
2023-03-30 06:58:48 +0900

    contact me

--- src/App.js
+++ src/App.js
@@ -84,6 +84,10 @@ function App() {
             </div>
           </li>
         </ul>
+        {/* Contact Me */}
+        <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          내 연락처
+        </div>
       </main>
     </div>
   );
