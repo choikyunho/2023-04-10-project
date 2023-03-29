8abcfbaf2cd33cdd4902052a1ac2bfdb1d61464c
h662Olbm <h662@olbm.app>
2023-03-30 06:59:42 +0900

    footer

--- src/App.js
+++ src/App.js
@@ -88,6 +88,9 @@ function App() {
         <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
           내 연락처
         </div>
+        <footer className="max-w-screen-xl mx-auto flex justify-end items-center py-1 md:py-2">
+          2023, Designed by, h662
+        </footer>
       </main>
     </div>
   );
