b20fd618d8390a734c0606455b4eb8c615d81a7d
h662Olbm <h662@olbm.app>
2023-04-04 08:01:44 +0900

    project card props

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,14 +1,15 @@
-function PortfolioCard() {
+function PortfolioCard({ title, desc, image, href }) {
   return (
     <li className="bg-purple-100 w-60 h-72 rounded-xl">
-      <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-        프로젝트 이미지
+      <div className="w-full h-1/2 rounded-t-xl">
+        <img
+          className="w-full h-full object-cover rounded-t-xl"
+          src={image}
+          alt="project"
+        />
       </div>
-      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-      <div className="mt-2 mx-2">
-        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다. 눈에
-        미인을 때까지 굳세게 그들의 바이며,
-      </div>
+      <div className="text-xl font-bold mt-2 mx-2">{title}</div>
+      <div className="mt-2 mx-2">{desc}</div>
     </li>
   );
 }
